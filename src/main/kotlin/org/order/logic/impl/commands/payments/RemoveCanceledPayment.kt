15fc09a585e27213e99a85ae77bba58fe619a3de
alex-paniman <alexpaniman@gmail.com>
2020-02-14 23:54:25 +0200

    yet another payments cancellation fix

--- src/main/kotlin/org/order/logic/impl/commands/payments/RemoveCanceledPayment.kt
+++ src/main/kotlin/org/order/logic/impl/commands/payments/RemoveCanceledPayment.kt
@@ -3,6 +3,8 @@ package org.order.logic.impl.commands.payments
 import org.order.data.entities.Payment
 import org.order.data.entities.State
 import org.order.logic.commands.processors.CallbackProcessor
+import org.order.logic.corpus.Text
+import org.order.logic.impl.utils.appendMainKeyboard
 
 val REMOVE_CANCELED_PAYMENT = CallbackProcessor("remove-canceled-payment") { user, src, (paymentIdStr) ->
     val paymentId = paymentIdStr.toInt()
@@ -11,6 +13,10 @@ val REMOVE_CANCELED_PAYMENT = CallbackProcessor("remove-canceled-payment") { use
     if (payment.registered == null) {
         payment.delete()
         user.state = State.COMMAND
+
+        user.send(Text["canceled-payment-was-successful-removed"]){
+            appendMainKeyboard(user)
+        }
     }
 
     src.delete()
