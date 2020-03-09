48c398f5396c8950d1137b5c5db828661126586f
alex-paniman <alexpaniman@gmail.com>
2020-03-09 16:50:56 +0200

    clean up after activation

--- src/main/kotlin/org/order/logic/impl/commands/display/Help.kt
+++ src/main/kotlin/org/order/logic/impl/commands/display/Help.kt
@@ -8,9 +8,7 @@ import org.order.logic.commands.triggers.and
 import org.order.logic.commands.triggers.or
 import org.order.logic.corpus.Text
 
-private val HELP_TRIGGER =
-        (CommandTrigger(Text["help-command"]) or CommandTrigger("help-slash-command")) and
-                StateTrigger(COMMAND)
+private val HELP_TRIGGER = CommandTrigger(Text["help-command"]) and StateTrigger(COMMAND)
 
 val HELP = TriggerCommand(HELP_TRIGGER) { user, _ ->
     user.send(Text["help"])
