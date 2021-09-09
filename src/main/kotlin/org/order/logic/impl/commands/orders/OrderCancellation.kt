b3fb9e9f6a19a661f9cb6b14d32bd5dabef21409
Alexander Paniman <alexpaniman@gmail.com>
2021-09-09 03:18:19 +0300

    Add some debug data logging

--- src/main/kotlin/org/order/logic/impl/commands/orders/OrderCancellation.kt
+++ src/main/kotlin/org/order/logic/impl/commands/orders/OrderCancellation.kt
@@ -72,6 +72,10 @@ val ORDER_CANCELLATION_WINDOW = Window(WINDOW_MARKER, ORDER_CANCELLATION_WINDOW_
         else -> nowDate
     }
 
+    println("$weekStart")
+
+    println(client.orders)
+
     val ordersAfterNow = client.orders
             .filter { !it.canceled }
             .filter { !it.orderDate.isBefore(nowDate) }
@@ -79,6 +83,8 @@ val ORDER_CANCELLATION_WINDOW = Window(WINDOW_MARKER, ORDER_CANCELLATION_WINDOW_
                 searchMode || // <== People who can search can delete order in any time
                         it.orderDate != nowDate || LAST_ORDER_TIME.isAfter(nowTime)
             }
+
+    println("$ordersAfterNow")
 
     val ordersWithDate = (1..5)
             .fold(mutableMapOf<LocalDate, MutableList<Order>>()) { map, dayOfWeek ->
@@ -89,6 +95,8 @@ val ORDER_CANCELLATION_WINDOW = Window(WINDOW_MARKER, ORDER_CANCELLATION_WINDOW_
                     } += ordersAfterNow.filter { order -> order.orderDate == day }
                 }
             }
+
+    println("$ordersWithDate")
     // -------------------------------------
 
     show(Text["suggest-order-cancellation"]) {
