14b8f2e072a81070e50b1214c7ea141e5c2f6b6a
alexpaniman <alexpaniman@gmail.com>
2024-09-22 21:00:18 +0300

    feature: limit range of poll rates on database level

--- src/main/kotlin/org/order/data/tables/PollAnswers.kt
+++ src/main/kotlin/org/order/data/tables/PollAnswers.kt
@@ -1,9 +1,12 @@
 package org.order.data.tables
 
 import org.jetbrains.exposed.dao.IntIdTable
+import org.jetbrains.exposed.sql.and
 
 object PollAnswers: IntIdTable("POLL_ANSWERS") {
     val order = reference("order_id", Orders)
     val dish = reference("dish_id", Dishes)
-    val rate = integer("rate")
+    val rate = integer("rate").check {
+        (it greaterEq 1) and (it lessEq  5)
+    }
 }
